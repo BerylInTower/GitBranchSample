fad46f2a88741983830c7640c2aa4d11644c66e4
Hsuan <wendy22388@gmail.com>
2023-01-07 14:12:58 +0800

    Add Room class By Leader

--- GitBranchApp/src/GitBranchApp1st/MainApp.java
+++ GitBranchApp/src/GitBranchApp1st/MainApp.java
@@ -5,6 +5,16 @@ public class MainApp {
 	public static void main(String[] args) {
 		// By Leader
 		System.out.println("1st version --By Leader");
+		// Update By Leader
+		Room room = new Room();
+		System.out.println();
+		System.out.println(room.add(10, 20));
 	}
 
+}
+
+class Room {
+	int add(int roomid, int orderid) {
+		return roomid + orderid;
+	}
 }
